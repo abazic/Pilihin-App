1404ae5980cb1a60afe3775d5517045dce848388
abazic <abdulazizichsanuddin@gmail.com>
2026-09-24 14:38:00 +0300

    Remove unused generateStaticParams function

--- app/gallery/[slug]/page.jsx
+++ app/gallery/[slug]/page.jsx
@@ -1,9 +1,5 @@
 import GalleryClient from './GalleryClient';
 
-export async function generateStaticParams() {
-  return [{ slug: 'demo' }];
-}
-
 export default function Page() {
   return <GalleryClient />;
 }
