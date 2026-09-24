55a2db2c8501093601d9a843d0e50bb3c665f7b6
abazic <abdulazizichsanuddin@gmail.com>
2026-09-24 14:37:16 +0300

    Remove output configuration from next.config.ts
    Removed output configuration to ensure proper build.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export', <-- HAPUS ATAU COMMENT BARIS INI
+  // Pastikan baris output: 'export' dihapus sama sekali
   images: {
     unoptimized: true,
   },
